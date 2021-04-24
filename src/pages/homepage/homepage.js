7980f98201982c13c1faa2357db002e1b4e26ded
satyam gharewal <gharewalsatyam996@gmail.com>
2021-04-24 15:19:31 +0530

    Initial Setup of Website

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import { React, Component } from 'react';
+
+class Homepage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <></>;
+  }
+}
+export default Homepage;
